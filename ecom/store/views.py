a1023f245b7bfe9615ff2c9079d73d8674096000
Bigcujo <princefrancisogbogu>
2025-01-13 21:43:28 -0800

    sales tag

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
+
+
+# home view 
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products':products})
+
+# about view
+def about(request):
+    return render(request, 'store/about.html', {})
